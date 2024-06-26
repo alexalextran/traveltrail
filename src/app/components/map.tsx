df55dfd5a6a7982a661ad093e116392e2a241470
Alex Tran <alextran8385@gmail.com>
2024-06-26 14:20:40 +1000

    the users current location is also shown on the map

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, AdvancedMarker } from '@vis.gl/react-google-maps';
 import Pin from './pin.tsx';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectPins } from '../store/pins/pinsSlice.ts';
@@ -7,6 +7,8 @@ import { fetchPins } from '../store/pins/pinsSlice.ts';
 import { useEffect, useState, useCallback } from 'react';
 import { AppDispatch } from '../store/store.ts'; // Import the AppDispatch type
 import useGeolocation from '../hooks/useGeolocation.ts'; // Import the custom hook
+import { MdPersonPinCircle } from "react-icons/md";
+import styles from '../Sass/page.module.scss';
 
 const MapComponent = () => {
   const dispatch: AppDispatch = useDispatch(); // Use the typed version of useDispatch
@@ -34,6 +36,10 @@ const MapComponent = () => {
           // Render each pin using the Pin component
           <Pin key={pin.id} pinID={pin.id} lat={pin.lat} lng={pin.lng} />
         ))}
+
+<AdvancedMarker position={{ lat: location.lat, lng: location.lng}}>
+<MdPersonPinCircle className={styles.svg}/>
+  </AdvancedMarker>
       </Map>
     </APIProvider>
   );
