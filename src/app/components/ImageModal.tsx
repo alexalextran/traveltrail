ce8bad48b69242245261a2b6c11c6d5e990fbd5d
Alex Tran <alextran8385@gmail.com>
2024-07-10 16:42:38 +1000

    imrproved styling for the image carousel

--- src/app/components/ImageModal.tsx
+++ src/app/components/ImageModal.tsx
@@ -4,11 +4,36 @@ import { selectSelectedPin } from '../store/pins/pinsSlice';
 import styles from "../Sass/modal.module.scss";
 import { removeImageReferenceFromFirestore } from '../firebaseFunctions/writeDocument';
 import { removeImageFromPin } from '../store/pins/pinsSlice';
+import Carousel from "react-multi-carousel";
 
 export default function ImageModal() {
     const selectedPin = useSelector(selectSelectedPin);
     const [images, setImages] = useState<string[]>([]); // Initialize with an empty array
     const dispatch = useDispatch();
+    const [imageFullscreen, setimageFullscreen] = useState(false)
+
+
+    const responsiveConfig = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 1
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 1
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 1
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        
+      }
+      }
+
 
     useEffect(() => {
         // Update images state when selectedPin.imageUrls changes
@@ -30,16 +55,38 @@ export default function ImageModal() {
     };
 
     return (
+        <>
+        
         <div className={styles.imageModal}>
             {images && images.map((url, index) => (
                 <div key={index} className={styles.imageDiv}>
                     <img src={url} alt={`Image ${index}`} style={{ width: '100px', height: 'auto' }} />
                     <div>
                         <a onClick={() => handleDeleteImage(url)}>Delete</a>
+                        <a onClick={() => setimageFullscreen(true)}>FullScreen</a>
                         <a href={url} target='_blank' rel='noopener noreferrer'>Download</a>
                     </div>
                 </div>
             ))}
         </div>
+
+        {images.length === 0 && <p>No images to display</p>}
+
+        {imageFullscreen && 
+        <div className={styles.imageModalFullScreen}>
+           <div className={styles.FSmodal}>
+        <Carousel 
+        responsive={responsiveConfig} 
+        className={styles.carousel} 
+        autoPlaySpeed={3000}
+        
+        >
+          {images.map((url, index) => (
+            <img key={index} src={url} alt="" />
+          ))}
+        </Carousel>
+      </div>
+        </div>}
+        </>
     );
 }
