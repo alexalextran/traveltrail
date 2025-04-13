d80717b3ab74b860fe1a325fc32995c6ba5be9fd
Alex Tran <alextran8385@gmail.com>
2025-04-13 18:42:28 +1000

    Vercel Analaytics Library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 // app/layout.tsx
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Providers from '../app/components/Providers';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Providers from "../app/components/Providers";
+import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Travel Trail',
-  description: 'Plan Your Next Trip!',
-  
+  title: "Travel Trail",
+  description: "Plan Your Next Trip!",
 };
 
 export default function RootLayout({
@@ -20,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} style={{ overflow: "hidden" }}>
-          <Providers>{children}</Providers>
+        <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
